b72f62cf48d8e3f1974efc13e329f2969543c46f
ashishkalsi07 <ashishkalsi07@gmail.com>
2025-04-22 22:09:27 +0530

    Completed

--- frontend/src/pages/Sigin.tsx
+++ frontend/src/pages/Sigin.tsx
@@ -2,7 +2,7 @@ import { Quote } from '../components/Quote'
 import Auth from '../components/Auth'
 const Signup = () => {
   return (
-    <div className='grid grid-cols-2'>
+    <div className='grid grid-cols-1 lg:grid-cols-2'>
         <div className='col-span-1'><Auth type="signin" /></div>
         <div className='col-span-1 invisible lg:visible'><Quote /></div>
     </div>
