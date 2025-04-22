36406d9daa36dd8f2be5b41981c1238c2fcc3818
ashishkalsi07 <ashishkalsi07@gmail.com>
2025-04-22 19:39:08 +0530

    Completed

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,12 +4,12 @@ export const Quote = () => {
             <div className="flex justify-center">
                 <div className="max-w-lg">
                     <div className="font-bold text-3xl">
-                        "Hey, I am Ashish Kalsi, a software engineer and entrepreneur. I am the founder of Kalsi Technologies, a company that specializes in building web and mobile applications. I am passionate about technology and love to share my knowledge with others."
+                        "Hey, I am Ashish Kalsi, a software engineer and I have created something amazing. This application is all about the different treks of Himachal Pradesh. Users can read about various treks and also contribute their own experiences by adding new treks as blogs."
                     </div> <div className="max-w-md font-semibold text-xl mt-4 ">
-                        Ashish Kalsi
+                        Created & Maintained By Ashish Kalsi
                     </div>
                     <div className="max-w-md font-semibold text-sm text-gray-500">
-                        CEO of Kalsi Technologies
+                        Software Engineer 
                     </div>
                 </div>
             </div>
